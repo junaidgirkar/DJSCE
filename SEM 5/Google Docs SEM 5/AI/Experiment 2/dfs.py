0a5a6186d7df71fe66fbad4a6a8ee5edf9115a14
Junaid Girkar <60307291+junaidgirkar@users.noreply.github.com>
2022-07-12 20:13:51 +0530

    Google Docs of Sem 5 n 6

---
+++ SEM 5/Google Docs SEM 5/AI/Experiment 2/dfs.py
@@ -0,0 +1,34 @@
+def dfs(graph, source,path = []):
+
+       if source not in path:
+
+           path.append(source)
+
+           if source not in graph:# leaf node, backtrack
+               return path
+
+           for neighbour in graph[source]:
+
+               path = dfs(graph, neighbour, path)
+
+
+       return path
+
+# graph = {"A":["B","C", "D"],
+#            "B":["E"],
+#            "C":["F","G"],
+#            "D":["H"],
+#            "E":["I"],
+#            "F":["J"]}
+# graph = {
+#   '5' : ['3','7'],
+#   '3' : ['2', '4'],
+#   '7' : ['8'],
+#   '2' : [],
+#   '4' : ['8'],
+#   '8' : []
+# }
+# print("Following is the Depth-First Search")
+# path = dfs(graph, "5")
+
+# print(" ".join(path))
---
+++ SEM 5/Google Docs SEM 5/AI/Experiment 2/driver.py
@@ -0,0 +1,49 @@
+from bfs import bfs
+from dfs import dfs
+
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+#         5
+#       /   \  
+#     3       7
+#    /  \      \
+#   2    4 ---- 8
+
+lecture_graph = {
+    '1' : ['4', '2'],
+    '4' : ['3'],
+    '3' : ['10', '9', '2'],
+    '10' :[],
+    '9' : [],
+    '2' : ['5', '8'],
+    '5' : ['2', '6','7'],
+    '7' : ['5', '8'],
+    '8' : ['2', '7'],
+    '6' : []
+}
+
+#     1
+#   /   \
+# 4       2
+#   \   / | \
+#     3   5   8
+#   / |   | \ |
+# 10  9   6   7      
+         
+# BFS
+visited = []
+print("Breadth-First Search traversal sequence")
+bfs(visited, lecture_graph, '1', goal_state='8')
+print("\n")
+
+#DFS
+print("Depth-First Search traversal sequence")
+path = dfs(lecture_graph, "1")
+print(" ".join(path))
